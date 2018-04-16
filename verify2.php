c53016995ce79ca7f7dc3a14cca896260d4ce6c7
Lloyd Goldstein <lgoldstein1@drew.edu>
2018-04-16 13:32:56 -0400

    vote totals v1

--- verify2.php
+++ verify2.php
@@ -6,7 +6,7 @@ if ($password == 24601) {
            window.location = "vmVoter.php";
            </script>';
 }
-elseif ($password == 42) {
+elseif ($password == 8675309) {
     echo "Correct admin password, ending voting session...";
     echo '<script type="text/javascript">
            window.location = "vmVoteTotal.php";
