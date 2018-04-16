29743590c63634be070b9cbd16da93ac48b4dbd2
Lloyd Goldstein <lgoldstein1@drew.edu>
2018-04-16 13:40:44 -0400

    vote totals v1

--- vmVoteTotal.php
+++ vmVoteTotal.php
@@ -15,23 +15,23 @@
 <body>
 <div class="header">Voting Session Concluded</div>
 <p>Vote Results</p>
-First Vote
-Mercuri: 3
-Castellucio: 2
-Biden: 3
-Squirrel: 1
-Cookie: 7
+First Vote<br>
+Mercuri: 3<br>
+Castellucio: 2<br>
+Biden: 3<br>
+Squirrel: 1<br>
+Cookie: 7<br>
 <p></p>
-Second Vote
-Hill: 5
-Juall: 4
-Dog: 7
-Caldwell: 4
-Fries: 12
+Second Vote<br>
+Hill: 5<br>
+Juall: 4<br>
+Dog: 7<br>
+Caldwell: 4<br>
+Fries: 12<br>
 <p></p>
-Third Vote
-Yes: 24
-No: 5
+Third Vote<br>
+Yes: 24<br>
+No: 5<br>
 <p></p>
 <a class="btn btn-primary" href="index.php" name="home">Back To Home</a>
 </body>
